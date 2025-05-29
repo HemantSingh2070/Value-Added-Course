5d2cba9eeea71168dc527ade6a7f6e54e445a53a
HemantSingh2070 <hemantsingh2070@gmail.com>
2025-05-29 16:47:04 +0530

    add

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 import homeRouter from "./routes/home.js";
 import formRouter from "./routes/form.js";
+
+// For __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,8 +30,9 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
+// Set view engine and views directory
+app.set("view engine", "ejs"); // or pug, hbs, etc.
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(PORT, () => {
   console.log(`Server is running.`);

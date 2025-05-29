00cf1df06c6869355adae2f8d659aa2b1b30301f
HemantSingh2070 <hemantsingh2070@gmail.com>
2025-05-29 16:17:12 +0530

    deployement time

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const homeRouter = require("./routes/home");
 const formRouter = require("./routes/form");
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", "./view");
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running.`);
 });
 
 app.use("/", homeRouter);
